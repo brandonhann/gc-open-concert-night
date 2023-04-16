4b9cb44ff1ed49944f4ced6808831f4d628379f0
Brandon Hann <bhann1999@gmail.com>
2023-04-16 14:09:30 -0400

    carousel img credit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ResponsiveCarousel = () => {
     const images = [
-        { src: "/slide1.jpg", alt: "Performance 1" },
-        { src: "/slide2.jpg", alt: "Performance 2" },
-        { src: "/slide3.jpg", alt: "Performance 3" },
+        { src: "/slide1.jpg", alt: "Concert 1" },
+        { src: "/slide2.jpg", alt: "Concert 2" },
+        { src: "/slide3.jpg", alt: "Concert 3" },
     ];
+
+    const [textVisible, setTextVisible] = useState(Array(images.length).fill(false));
+
+    const handleImageClick = (index) => {
+        const newTextVisible = [...textVisible];
+        newTextVisible[index] = !newTextVisible[index];
+        setTextVisible(newTextVisible);
+    };
 
     return (
         <section className="fade text-blue-900 rounded-lg mb-8">
@@ -53,13 +61,15 @@ const ResponsiveCarousel = () => {
                     }
                 >
                     {images.map((image, index) => (
-                        <div key={index}>
+                        <div key={index} onClick={() => handleImageClick(index)}>
                             <img
                                 className="rounded-lg shadow-md object-cover w-full h-auto"
                                 src={image.src}
                                 alt={image.alt}
                             />
-
+                            {textVisible[index] && (
+                                <p className='absolute top-1 ml-2 text-sm text-blue-50 bg-black bg-opacity-75 px-2 rounded-md'>Image generated using DALL·E 2</p>
+                            )}
                         </div>
                     ))}
                 </Carousel>
@@ -67,11 +77,14 @@ const ResponsiveCarousel = () => {
                     {images.map((image, index) => (
                         <div className="flex-1" key={index}>
                             <img
-                                className="rounded-lg shadow-md object-cover w-full h-auto m-auto"
+                                className="rounded-lg cursor-pointer shadow-md object-cover w-full h-auto m-auto"
                                 src={image.src}
                                 alt={image.alt}
+                                onClick={() => handleImageClick(index)}
                             />
-
+                            {textVisible[index] && (
+                                <p className='absolute bottom-1 ml-2 text-sm text-blue-50 bg-black bg-opacity-75 px-2 rounded-md'>Image generated using DALL·E 2</p>
+                            )}
                         </div>
                     ))}
                 </div>
